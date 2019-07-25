c7c35027deec17581a894c248595ed07ca76a818
xc8801 <xc8801@126.com>
2019-07-24 22:39:02 -0700

    Disable mergify and CircleCi (#39)
    - Also update common files and retire custom go-bindata.sh script

--- scripts/run_gobindata.sh
+++ scripts/run_gobindata.sh
@@ -1,4 +1,11 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
+# WARNING: DO NOT EDIT, THIS FILE IS PROBABLY A COPY
+#
+# The original version of this file is located in the https://github.com/istio/common-files repo.
+# If you're looking at this file in a different repo and want to make a change, please go to the
+# common-files repo, make the change there and check it in. Then come back to this repo and run
+# "make updatecommon".
 
 # Copyright 2018 Istio Authors
 #
@@ -13,6 +20,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+set -e
 
 SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ROOTDIR=$(dirname "${SCRIPTPATH}")
