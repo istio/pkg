8803eb130b6ae51d693185ad77bfead83f116c01
therealmitchconnors <mitchconnors@gmail.com>
2019-10-04 13:34:00 -0700

    Update copyright

--- ledger/smt.go
+++ ledger/smt.go
@@ -1,11 +1,21 @@
-/**
- *  @file
- *  @copyright defined in aergo/LICENSE.txt
- */
+// Copyright 2019 Istio Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 package ledger
 
-// The Package Trie implements a sparse merkle trie.
+// The SMT is derived from https://github.com/aergoio/SMT with modifications
+// to remove unneeded features, and to support retention of old nodes for a fixed time.
 
 import (
 	"bytes"
